0c156675865b49843bf5ac442a6b8e0756367e72
a0merr <a0merr05@louisville.edu>
2025-09-14 19:51:14 -0400

    LLUList.h

--- LLUList.h
+++ LLUList.h
@@ -1,65 +1,21 @@
-#include "LLUList.h"
+#ifndef LLULIST_H
+#define LLULIST_H
 #include <iostream>
 #include <stdexcept>
-using namespace std;
-	LLUList::LLUList() {
-    head = nullptr;
-}
-	LLUList::~LLUList() {
-    Node *current = head;
-    while (current) {
-        Node *temp = current;
-        current = current->next;
-        delete temp;
-    }
-}
-	void LLUList::insert(int value) {
-    Node *newNode = new Node(value);
-    if (!head) {
-        head = newNode;
-    } else {
-        Node *temp = head;
-        while (temp->next) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }
-}
-	int LLUList::getMin() const {
-    if (!head)
-        throw runtime_error("List is empty!");
-
-    int minVal = head->data;
-    Node *temp = head->next;
-    while (temp) {
-        if (temp->data < minVal)
-            minVal = temp->data;
-        temp = temp->next;
-    }
-    return minVal;
-}
-	int LLUList::getRange() const {
-    if (!head)
-        throw runtime_error("List is empty!");
-
-    int minVal = head->data;
-    int maxVal = head->data;
-
-    Node *temp = head->next;
-    while (temp) {
-        if (temp->data < minVal)
-            minVal = temp->data;
-        if (temp->data > maxVal)
-            maxVal = temp->data;
-        temp = temp->next;
-    }
-    return maxVal - minVal;
-}
-	void LLUList::display() const {
-    Node *temp = head;
-    while (temp) {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
+class LLUList {
+private:
+    struct Node {
+        int data;
+        Node* next;
+        Node(int val) : data(val), next(nullptr) {}
+    };
+    Node* head;
+public:
+    LLUList();
+    ~LLUList();
+    void insert(int value);
+    int getMin() const;
+    int getRange() const;
+    void display() const;
+};
+#endif
