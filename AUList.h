92c21404b6d307bcb44914bed872415e2b74a4ee
a0merr <a0merr05@louisville.edu>
2025-09-14 10:27:57 -0400

    AUList.h

---
+++ AUList.h
@@ -0,0 +1,30 @@
+#ifndef AULIST_H
+#define AULIST_H
+
+#include <stdexcept>
+
+class AUList {
+private:
+    int* data;      
+    int size;       
+    int capacity;   
+
+public:
+    // Constructors & Destructor
+    AUList(int cap = 100);           
+    AUList(const AUList& other);     
+    ~AUList();                       
+
+    // Core functions
+    void insert(int val);            
+    int getMin() const;              
+    int getRange() const;            
+    AUList DuplicateSE(int first, int last);  
+
+    // Utility functions
+    int getSize() const { return size; }      
+    int getAt(int index) const;               
+};
+
+#endif
+
