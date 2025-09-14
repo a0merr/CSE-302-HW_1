b0e2eadb66daa0bb8dcfcf704c0228da3977b004
a0merr <a0merr05@louisville.edu>
2025-09-14 19:49:00 -0400

    AUList.h

--- AUList.h
+++ AUList.h
@@ -10,6 +10,7 @@ public:
     AUList(int cap = 100);
     AUList(const AUList& other);
     ~AUList();
+
     void insert(int val);
     int getMin() const;
     int getRange() const;
@@ -17,6 +18,7 @@ public:
     // Added missing functions used in your tests
     void MakeEmpty();
     void PutItem(int val);
+
     int getSize() const { return size; }
     int getAt(int index) const;
 };
